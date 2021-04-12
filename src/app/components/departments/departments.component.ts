40949d69d3bd4bbf64bf291e34d60868c6875aef
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-12 10:09:37 -0600

    Unit 6 Completed

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Department } from 'src/app/interfaces/department';
 import { DepartmentsService } from 'src/app/services/departments.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
@@ -10,9 +11,16 @@ import { DepartmentsService } from 'src/app/services/departments.service';
 export class DepartmentsComponent implements OnInit {
   departments: Department[];
 
-  constructor(private departmentsService: DepartmentsService) {}
+  constructor(
+    private departmentsService: DepartmentsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
+  }
+
+  goToDepartment(departmentId: string): void {
+    this.router.navigate(['./timesheet', { id: departmentId }]);
   }
 }
