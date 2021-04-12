48f740aed4a6a30393a51da883f2a926092e5a17
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-12 11:07:44 -0600

    Completed Unit 14

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -39,6 +39,12 @@ export class TimesheetComponent implements OnInit {
     this.department = this.departments.find(
       (department) => department.id === this.route.snapshot.params['id']
     );
+
+    this.employeeService
+      .getEmployeeHoursByDepartment(this.department.id)
+      .subscribe((employees) => {
+        this.employees = employees;
+      });
   }
 
   addEmployee(): void {
@@ -89,13 +95,21 @@ export class TimesheetComponent implements OnInit {
     );
   }
 
-  deleteEmployee(index: number): void {
+  deleteEmployee(employee: Employee, index: number): void {
+    if (employee.id) {
+      this.employeeService.deleteEmployeeHours(employee);
+    }
+
     this.employees.splice(index, 1);
   }
 
   submit(): void {
     this.employees.forEach((employee) => {
-      this.employeeService.saveEmployeeHours(employee);
+      if (employee.id) {
+        this.employeeService.updateEmployeeHours(employee);
+      } else {
+        this.employeeService.saveEmployeeHours(employee);
+      }
     });
 
     this.router.navigate(['./departments']);
