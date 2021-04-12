a0d5b869b041c249062623452359159a2d1525e3
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-12 10:01:15 -0600

    Unit 5 Completed

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
-  styleUrls: ['./departments.component.scss']
+  styleUrls: ['./departments.component.scss'],
 })
 export class DepartmentsComponent implements OnInit {
+  departments: Department[];
 
-  constructor() { }
+  constructor(private departmentsService: DepartmentsService) {}
 
   ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
   }
-
 }
