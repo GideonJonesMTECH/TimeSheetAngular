6ceddac480acbb59a15315280542fefee295ba1d
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-12 10:52:02 -0600

    Completed Unit 12

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Department } from 'src/app/interfaces/department';
 import { DepartmentsService } from 'src/app/services/departments.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -10,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class DepartmentsComponent implements OnInit {
   departments: Department[];
+  $departments: Observable<Department[]>;
 
   constructor(
     private departmentsService: DepartmentsService,
@@ -17,9 +19,10 @@ export class DepartmentsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe((departments) => {
-      this.departments = departments;
-    });
+    // this.departmentsService.getDepartments().subscribe((departments) => {
+    //   this.departments = departments;
+    // });
+    this.$departments = this.departmentsService.getDepartments();
   }
 
   goToDepartment(departmentId: string): void {
